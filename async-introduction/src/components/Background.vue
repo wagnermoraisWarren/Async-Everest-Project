604d6c4963677182aa3c85c60b41c514354ff868
wagnermoraisWarren <wagner.morais@warren.com.br>
2023-02-17 17:31:37 -0300

    teste

--- async-introduction/src/components/Background.vue
+++ async-introduction/src/components/Background.vue
@@ -17,6 +17,6 @@ export default {
 
     background-position: center;
     background-size: cover;
-    box-shadow: 1px 2px 5px rgba(0, 0, 0, .4);
+    box-shadow: 1px 2px 10px rgba(0, 0, 0, .4);
 }
 </style>
