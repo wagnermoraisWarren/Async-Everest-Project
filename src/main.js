d6d46d2c2efd117f8f9c249db8713ec917cb8dc5
wagnermoraisWarren <wagner.morais@warren.com.br>
2023-02-27 17:29:31 -0300

    feat: novas dependencias e rotas

--- src/main.js
+++ src/main.js
@@ -2,10 +2,59 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueMask from 'v-mask'
-import { Server, Model } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 Vue.config.productionTip = false
 Vue.use(VueMask);
+
+createServer ({
+
+  models: {
+    car: Model
+  },
+
+  seeds(server) {
+    server.create("car", { plate: "IYL-3973", model: "Ka", color: "Branco", year: "2018", conc: "Ford" })
+    server.create("car", { plate: "IYN-3997", model: "Onix", color: "Laranja", year: "2022", conc: "Chevrolet" })
+    server.create("car", { plate: "IWK-3369", model: "Civic", color: "Preto", year: "2019", conc: "Honda" })
+  },
+
+  routes() {
+    this.namespace = 'api'
+    
+    this.get('/list', (schema, request) => {
+      return schema.cars.all()
+    }, { timing: 1000 } )
+
+    this.post('/list', (schema, request) => {
+      let car = JSON.parse(request.requestBody)
+      return schema.cars.create(car)
+    })
+
+    this.get('/list/:id', (schema, request) => {
+      let data = JSON.parse(request.requestBody)
+      let id = request.params.id
+      let car = schema.cars.find(id)
+
+      return car.update(data)
+    }, { timing: 2000 })
+  }
+})
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faArrowLeft, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPenToSquare, faTrash, faArrowLeft)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Formulario from '../components/Formulario.vue'
+import Editar from '../components/Editar.vue'
+import Lista from '../components/Lista.vue'
 
 Vue.use(VueRouter)
 
@@ -11,10 +13,23 @@ const routes = [
     name: 'home',
     component: Home
   },
+
   {
     path: '/register',
     name: 'register',
     component: Formulario
+  },
+
+  {
+    path: '/edit',
+    name: 'edit',
+    component: Editar
+  },
+
+  {
+    path: '/list',
+    name: 'list',
+    component: Lista
   }
 ]
 
