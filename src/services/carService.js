ddf2da8c10eff26aa519b0269b81ae32495e2ca0
wagnermoraisWarren <wagner.morais@warren.com.br>
2023-03-01 17:37:17 -0300

    feat: criando arquivos services

---
+++ src/services/carService.js
@@ -0,0 +1,19 @@
+import { http } from "./config";
+
+export default {
+  get: async () => {
+    const response = await http.get("/list");
+    return response;
+  },
+  delete: async (id) => {
+    return http.delete(`/list/${id}`);
+  },
+  post: async (request) => {
+    const response = await http.post("/list/", request)
+    return response
+  },
+  put: async (request) => {
+    const response = await http.put(`/list/${request.id}/`, request)
+    return response
+  }
+};
